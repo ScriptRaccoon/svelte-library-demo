4fe96fe295d7df75506c16454e00cba3e9e6d157
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 01:37:36 +0200

    create layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import './app.css'
+</script>
+
+<header>
+	<h1>Svelte Library Demo</h1>
+</header>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+	</ul>
+</nav>
+
+<main>
+	<slot></slot>
+</main>
+
+<style>
+	header {
+		text-align: center;
+		padding-top: 1rem;
+	}
+
+	nav {
+		padding-block: 0.5rem;
+	}
+
+	ul {
+		display: flex;
+		justify-content: center;
+		list-style: none;
+		gap: 1rem;
+	}
+
+	main {
+		max-width: 54rem;
+		margin-inline: auto;
+		padding-inline: 1rem;
+	}
+</style>
