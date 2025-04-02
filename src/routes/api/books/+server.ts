5051966791235cd2143b9e486d52a330411ada08
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 23:39:14 +0200

    refactor

--- src/routes/api/books/+server.ts
+++ src/routes/api/books/+server.ts
@@ -6,19 +6,12 @@ import { BookListSchema, type BookList } from '$lib/schemas'
 export const GET: RequestHandler = async (event) => {
 	const genre_id = event.url.searchParams.get('genre_id')
 	try {
-		let books: BookList = []
-
-		if (genre_id) {
-			const { rows } = await db.execute(
-				'SELECT id, title FROM books WHERE genre_id = :genre_id',
-				{ genre_id },
-			)
-			books = BookListSchema.parse(rows)
-		} else {
-			const { rows } = await db.execute('SELECT id, title FROM books')
-			books = BookListSchema.parse(rows)
-		}
-
+		const query = genre_id
+			? 'SELECT id, title FROM books WHERE genre_id = :genre_id'
+			: 'SELECT id, title FROM books'
+		const args = genre_id ? { genre_id } : undefined
+		const { rows } = await db.execute(query, args)
+		const books: BookList = BookListSchema.parse(rows)
 		return json(books)
 	} catch (err) {
 		console.error(err)
