86b5369a104907b171f2f278390538fe19ce2b10
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 17:30:44 +0200

    separate header comp

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<header>
+	<h1>Svelte Library Demo</h1>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/books">Books</a>
+			</li>
+			<li>
+				<a href="/genres">Genres</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		text-align: center;
+		padding-block: 1.25rem;
+		background-color: var(--primary-color);
+	}
+
+	nav {
+		margin-top: 0.25rem;
+	}
+
+	ul {
+		display: flex;
+		justify-content: center;
+		list-style: none;
+		gap: 1rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,15 @@
 <script lang="ts">
+	import Header from '$lib/components/Header.svelte'
 	import './app.css'
 </script>
 
-<header>
-	<h1>Svelte Library Demo</h1>
-
-	<nav>
-		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="/books">Books</a>
-			</li>
-			<li>
-				<a href="/genres">Genres</a>
-			</li>
-		</ul>
-	</nav>
-</header>
+<Header />
 
 <main>
 	<slot></slot>
 </main>
 
 <style>
-	header {
-		text-align: center;
-		padding-block: 1.25rem;
-		background-color: var(--primary-color);
-	}
-
-	nav {
-		margin-top: 0.25rem;
-	}
-
-	ul {
-		display: flex;
-		justify-content: center;
-		list-style: none;
-		gap: 1rem;
-	}
-
 	main {
 		max-width: 42rem;
 		margin-inline: auto;
