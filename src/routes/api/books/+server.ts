f8e2c77dce172ded39d210ce6d2a8e69169704d7
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 23:45:47 +0200

    expand /api/books to filter for author

--- src/routes/api/books/+server.ts
+++ src/routes/api/books/+server.ts
@@ -5,16 +5,27 @@ import { BookListSchema, type BookList } from '$lib/schemas'
 
 export const GET: RequestHandler = async (event) => {
 	const genre_id = event.url.searchParams.get('genre_id')
+	const author_id = event.url.searchParams.get('author_id')
 	try {
-		const query = genre_id
-			? 'SELECT id, title FROM books WHERE genre_id = :genre_id'
-			: 'SELECT id, title FROM books'
-		const args = genre_id ? { genre_id } : undefined
-		const { rows } = await db.execute(query, args)
+		const query = get_query(genre_id, author_id)
+		const { rows } = await db.execute(query, { genre_id, author_id })
 		const books: BookList = BookListSchema.parse(rows)
 		return json(books)
 	} catch (err) {
 		console.error(err)
 		return error(500, 'Cannot fetch books')
 	}
+}
+
+function get_query(genre_id: string | null, author_id: string | null): string {
+	if (genre_id && author_id) {
+		return 'SELECT id, title FROM books WHERE genre_id = :genre_id AND author_id = :author_id'
+	}
+	if (genre_id && !author_id) {
+		return 'SELECT id, title FROM books WHERE genre_id = :genre_id'
+	}
+	if (author_id && !genre_id) {
+		return 'SELECT id, title FROM books WHERE author_id = :author_id'
+	}
+	return 'SELECT id, title FROM books'
 }
