e1e8d883a14c2343daae45b56f0961eba9b33698
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 02:05:16 +0200

    guard fetch error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,16 @@
+import { error } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
+
+type BookTitle = {
+	id: number
+	title: string
+}
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const res = await fetch('/api/titles')
-	const books = (await res.json()) as { id: number; title: string }[]
+	if (!res.ok) {
+		return error(res.status, 'Failed to fetch books')
+	}
+	const books: BookTitle[] = await res.json()
 	return { books }
 }
