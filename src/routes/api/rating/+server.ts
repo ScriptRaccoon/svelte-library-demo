ec161465947b0244aad37ecc90858dae8ac28828
Script Raccoon <scriptraccoon@gmail.com>
2025-04-02 04:40:11 +0200

    add post endpoint to submit score

---
+++ src/routes/api/rating/+server.ts
@@ -0,0 +1,11 @@
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { db } from '$lib/server/db'
+
+export const POST: RequestHandler = async (event) => {
+	const body = await event.request.json()
+	const { book_id, rating } = body
+	const sql = 'INSERT INTO ratings (book_id, rating) VALUES (:book_id, :rating)'
+	await db.execute(sql, { book_id, rating })
+	return json({ message: 'Rating received' })
+}
