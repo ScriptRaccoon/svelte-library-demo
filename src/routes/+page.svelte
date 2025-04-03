09b7a511a3a1d2178542ee116c75aba781a8943e
Script Raccoon <scriptraccoon@gmail.com>
2025-04-03 03:08:24 +0200

    add github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <h2>Home</h2>
 
 <p>This is a demo on how to work with SQLite and SvelteKit.</p>
+
+<p>
+	The code is on <a
+		href="https://github.com/ScriptRaccoon/svelte-library-demo"
+		target="_blank">GitHub</a
+	>.
+</p>
