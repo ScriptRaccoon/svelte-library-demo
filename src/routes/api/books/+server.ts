cd9d2e5392d8c0a0e63a413b078aeb54c3e823b7
Script Raccoon <scriptraccoon@gmail.com>
2025-04-03 02:08:18 +0200

    fix query

--- src/routes/api/books/+server.ts
+++ src/routes/api/books/+server.ts
@@ -1,31 +1,35 @@
 import type { RequestHandler } from './$types'
 import { error, json } from '@sveltejs/kit'
 import { db } from '$lib/server/db'
-import { BookListSchema, type BookList } from '$lib/schemas'
+import { BookListSchema } from '$lib/schemas'
+
+function query_books(genre_id: string | null, author_id: string | null) {
+	if (genre_id && author_id) {
+		return db.execute(
+			'SELECT id, title FROM books WHERE genre_id = :genre_id AND author_id = :author_id',
+			{ genre_id, author_id },
+		)
+	} else if (genre_id && !author_id) {
+		return db.execute('SELECT id, title FROM books WHERE genre_id = :genre_id', {
+			genre_id,
+		})
+	} else if (!genre_id && author_id) {
+		return db.execute('SELECT id, title FROM books WHERE author_id = :author_id', {
+			author_id,
+		})
+	}
+	return db.execute('SELECT id, title FROM books')
+}
 
 export const GET: RequestHandler = async (event) => {
 	const genre_id = event.url.searchParams.get('genre_id')
 	const author_id = event.url.searchParams.get('author_id')
 	try {
-		const query = get_query(genre_id, author_id)
-		const { rows } = await db.execute(query, { genre_id, author_id })
-		const books: BookList = BookListSchema.parse(rows)
+		const { rows } = await query_books(genre_id, author_id)
+		const books = BookListSchema.parse(rows)
 		return json(books)
 	} catch (err) {
 		console.error(err)
 		return error(500, 'Cannot fetch books')
 	}
-}
-
-function get_query(genre_id: string | null, author_id: string | null): string {
-	if (genre_id && author_id) {
-		return 'SELECT id, title FROM books WHERE genre_id = :genre_id AND author_id = :author_id'
-	}
-	if (genre_id && !author_id) {
-		return 'SELECT id, title FROM books WHERE genre_id = :genre_id'
-	}
-	if (author_id && !genre_id) {
-		return 'SELECT id, title FROM books WHERE author_id = :author_id'
-	}
-	return 'SELECT id, title FROM books'
 }
